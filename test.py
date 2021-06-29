8bdf93e5d33f84611d6290e4fb50c4d9197097cb
junho3174 <kou2004@naver.com>
2021-06-29 14:11:04 +0900

    Default Changelist

--- test.py
+++ test.py
@@ -5,23 +5,36 @@ import json
 import xmltodict
 
 url = 'http://apis.data.go.kr/1192000/VsslEtrynd2/Info'
-queryParams = '?' + urlencode({ quote_plus('ServiceKey') : 'Kk6GfZmcpKlmvK0ufG8JnZs8nZhJhSjiEWY6Qs6SoOdLkZLcdNam6Fv8JJBMVKJaXUkCz3+6o3M4O1nW6YMdRw==', \
-                                quote_plus('pageNo') : '1', \
-                                quote_plus('numOfRows') : '10', \
-                                quote_plus('prtAgCd') : '030', \
-                                quote_plus('sde') : '20210629', \
-                                quote_plus('ede') : '20210630', \
-                                quote_plus('clsgn') : '' } \
+queryParams = '?' + urlencode({quote_plus(
+    'ServiceKey'): 'Kk6GfZmcpKlmvK0ufG8JnZs8nZhJhSjiEWY6Qs6SoOdLkZLcdNam6Fv8JJBMVKJaXUkCz3+6o3M4O1nW6YMdRw==', \
+                               quote_plus('pageNo'): '1', \
+                               quote_plus('numOfRows'): '50', \
+                               quote_plus('prtAgCd'): '030', \
+                               quote_plus('sde'): '20210629', \
+                               quote_plus('ede'): '20210629', \
+                               quote_plus('clsgn'): ''} \
                               )
-
+# 인증 key, 페이지 번호, 한 페이지 결과 수(최대 50), 항만청코드(인천항 030), 검색 시작일, 검색 종료일, 호출부호
 req = urlopen(url + queryParams)
 res = req.read().decode('utf8')
 dict_type = xmltodict.parse(res)
 json_type = json.dumps(dict_type)
 dict2_type = json.loads(json_type)
-pprint(dict2_type)
-
+# pprint(dict2_type["response"]["body"]["items"]["item"])
+i = 1
+for item in dict2_type["response"]["body"]["items"]["item"]:
+    print("No : " + str(i))
+    print("Vessel : " + item["vsslNm"])
+    print("선박국가 : " + item["vsslNltyNm"])
+    try:
+        print("ETA : " + item["details"]["detail"][0]["etryptDt"])
+        print("내외항 : " + item["details"]["detail"][0]["ibobprtNm"] + "\n")
+    except:
+        print("ETA : " + item["details"]["detail"]["etryptDt"])
+        print("내외항 : " + item["details"]["detail"]["ibobprtNm"] + "\n")
+    i += 1
+'''
 file_path = "./sample.json"
 with open(file_path, 'w', encoding='UTF-8') as outfile:
     json.dump(dict2_type, outfile, indent=4, ensure_ascii=False)
-
+'''
