6c6dba18770990b5524133cd54bfcba108c14b5a
junho3174 <kou2004@naver.com>
2021-07-01 17:31:48 +0900

    Default Changelist

--- getInsightSatlit.py
+++ getInsightSatlit.py
@@ -3,6 +3,8 @@ import urllib.request
 import json
 from urllib.request import urlretrieve
 import time
+from urllib.error import HTTPError
+from urllib.error import URLError
 
 url = 'http://apis.data.go.kr/1360000/SatlitImgInfoService/getInsightSatlit?' \
       'serviceKey=Kk6GfZmcpKlmvK0ufG8JnZs8nZhJhSjiEWY6Qs6SoOdLkZLcdNam6Fv8JJBMVKJaXUkCz3%2B6o3M4O1nW6YMdRw%3D%3D&' \
@@ -13,14 +15,20 @@ res = req.read()
 
 json_object = json.loads(res)
 json_object2 = json_object.get('response').get('body').get('items').get('item')[0].get('satImgC-file')
-json_object3 = json_object2[1:-1].split(',')
+json_object3 = json_object2[1:-1].split(',') # 주소값이 담긴 리스트 모양의 하나의 큰 문자열로 되어있음
 i = 0
 
 for image in json_object3:
-    urlretrieve(image.strip(), "C:/Users/LeeJunHo/Documents/GitHub/test/image/image"+str(i)+".png")
-    i += 1
-    time.sleep(0.5)
-    print("download :", i)
+    try:
+        urlretrieve(image.strip(), "C:/Users/LeeJunHo/Documents/GitHub/test/image/image"+str(i)+".png")
+        i += 1
+        #time.sleep(0.5)
+        print("download :", i)
+    except HTTPError as e:
+        print("HTTPError :", e)
+    except URLError as e:
+        print("URLError :", e)
 
 # urllib.error.HTTPError: HTTP Error 500: Internal Server Error
+# http.client.RemoteDisconnected: Remote end closed connection without response
 
