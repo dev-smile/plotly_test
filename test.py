0d0a0a805cc259c58778b3666f2740d471500927
junho3174 <kou2004@naver.com>
2021-07-01 09:35:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,16 +5,18 @@ import json
 import xmltodict
 
 url = 'http://apis.data.go.kr/1192000/VsslEtrynd2/Info'
-queryParams = '?' + urlencode({quote_plus(
-    'ServiceKey'): 'Kk6GfZmcpKlmvK0ufG8JnZs8nZhJhSjiEWY6Qs6SoOdLkZLcdNam6Fv8JJBMVKJaXUkCz3+6o3M4O1nW6YMdRw==', \
-                               quote_plus('pageNo'): '1', \
-                               quote_plus('numOfRows'): '50', \
-                               quote_plus('prtAgCd'): '030', \
-                               quote_plus('sde'): '20210629', \
-                               quote_plus('ede'): '20210629', \
-                               quote_plus('clsgn'): ''} \
-                              )
-# 인증 key, 페이지 번호, 한 페이지 결과 수(최대 50), 항만청코드(인천항 030), 검색 시작일, 검색 종료일, 호출부호
+queryParams = '?' + urlencode(
+    {
+        quote_plus('ServiceKey'): 'Kk6GfZmcpKlmvK0ufG8JnZs8nZhJhSjiEWY6Qs6SoOdLkZLcdNam6Fv8JJBMVKJaXUkCz3+6o3M4O1nW6YMdRw==', # 인증키
+        quote_plus('pageNo'): '1', # 페이지 번호
+        quote_plus('numOfRows'): '50', # 한 페이지 결과 수(최대 50)
+        quote_plus('prtAgCd'): '030', # 항만청코드(인천항 030)
+        quote_plus('sde'): '20210629', # 검색 시작일
+        quote_plus('ede'): '20210629', # 검색 종료일
+        quote_plus('clsgn'): '' # 호출부호
+    }
+)
+
 req = urlopen(url + queryParams)
 res = req.read().decode('utf8')
 dict_type = xmltodict.parse(res)
@@ -33,6 +35,7 @@ for item in dict2_type["response"]["body"]["items"]["item"]:
         print("ETA : " + item["details"]["detail"]["etryptDt"])
         print("내외항 : " + item["details"]["detail"]["ibobprtNm"] + "\n")
     i += 1
+
 '''
 file_path = "./sample.json"
 with open(file_path, 'w', encoding='UTF-8') as outfile:
